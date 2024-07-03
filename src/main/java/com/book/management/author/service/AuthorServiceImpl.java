c53d0abf574d76d2e1baac4c51cf763a7f903e87
John <john.oladeji@slsbank.com>
2024-07-03 15:01:39 +0100

    Implemented author service to handle crud operation

---
+++ src/main/java/com/book/management/author/service/AuthorServiceImpl.java
@@ -0,0 +1,58 @@
+package com.book.management.author.service;
+
+import com.book.management.author.domain.Author;
+import com.book.management.author.dto.AuthorRequestDto;
+import com.book.management.author.repository.AuthorRepository;
+import com.book.management.exception.ResourcesNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AuthorServiceImpl implements AuthorService{
+
+    private final AuthorRepository authorRepository;
+    private final ModelMapper modelMapper;
+    @Override
+    public List<Author> getAllAuthors() {
+        return authorRepository.findAll();
+    }
+    @Override
+    public Author getAuthorById(Long id) throws ResourcesNotFoundException {
+        return authorRepository.findById(id).orElseThrow(() -> new ResourcesNotFoundException("Author not found"));
+    }
+    @Override
+    public Author createAuthor(AuthorRequestDto authorRequestDto) {
+        log.info("create Author request : {}", authorRequestDto);
+
+        Author author = modelMapper.map(authorRequestDto, Author.class);
+        return authorRepository.save(author);
+    }
+    @Override
+    @Transactional
+    public Author updateAuthor(Long id, AuthorRequestDto authorDetails) throws ResourcesNotFoundException, IllegalAccessException {
+        log.info("update Author request : {}", authorDetails);
+        if (id <= 0) {
+            throw new IllegalAccessException("the id provided is invalid");
+        }
+        Author author = getAuthorById(id);
+        modelMapper.map(authorDetails,author);
+        Author updatedAuthor = authorRepository.save(author);
+        log.info("Author updated successfully : {}", updatedAuthor);
+
+        return updatedAuthor;
+
+    }
+    @Override
+    public void deleteAuthor(Long id) throws ResourcesNotFoundException {
+        Author author = getAuthorById(id);
+        authorRepository.delete(author);
+    }
+
+}
