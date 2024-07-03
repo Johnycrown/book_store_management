283025eefc62938d8d8bee9f06f10de55dbcfe00
John <john.oladeji@slsbank.com>
2024-07-03 14:40:58 +0100

    modified author request dto

---
+++ src/main/java/com/book/management/author/dto/AuthorRequestDto.java
@@ -0,0 +1,21 @@
+package com.book.management.author.dto;
+
+import com.book.management.book.domain.Book;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.List;
+import java.util.Set;
+
+@Data
+public class AuthorRequestDto {
+    private Long id;
+    private String name;
+    private List<String> awards;
+    private Set<Book> books;
+}
